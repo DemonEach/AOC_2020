136193d8b60c195e76089656d99c2f0a3d7bee7d
Dmitry Korenev <koreneffdmitrii@gmail.coom>
2020-12-30 10:49:25 +0500

    some format fixes

--- 01_rust/src/main.rs
+++ 01_rust/src/main.rs
@@ -1,12 +1,10 @@
 use std::fs;
 
-
-fn solve1(xs0: &Vec<u32>) -> u32 {
-
+fn solution(xs0: &Vec<u32>) -> u32 {
     let xs = xs0.clone();
 
     for i in 0..xs.len() {
-        if let Ok(j) = xs.binary_search(&(2020-xs[i])) {
+        if let Ok(j) = xs.binary_search(&(2020 - xs[i])) {
             if i != j {
                 return xs[i] * xs[j];
             }
@@ -15,19 +13,16 @@ fn solve1(xs0: &Vec<u32>) -> u32 {
     unreachable!()
 }
 
-
-
 fn main() {
     let file_name: String = "./sample-01.txt".to_string();
     let contents = fs::read_to_string(file_name).expect("Something went wrong reading the file");
     let mut numbers: Vec<u32> = contents
         .split("\n")
-        .filter(|s| !s.is_empty() )
+        .filter(|s| !s.is_empty())
         .map(|x| x.trim().parse::<u32>().unwrap())
         .collect();
     numbers.sort();
     println!("Numbers:{:?}", numbers);
 
-    println!("Value:{:?}", solve1(&numbers));
-    
+    println!("Value:{:?}", solution(&numbers));
 }
